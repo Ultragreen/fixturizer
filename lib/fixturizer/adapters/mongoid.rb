0dc9b16bec9bc779707f8ce3cf90830fb7bf53af
Romain Georges <romain.georges@orange.com>
2024-10-07 15:29:00 +0200

    correction on Mongoid adapter after changing link bu belong

--- lib/fixturizer/adapters/mongoid.rb
+++ lib/fixturizer/adapters/mongoid.rb
@@ -4,26 +4,27 @@ module Fixturizer
   module Adapters
     module Mongoid
       def inject_data
-        list = (@orders.is_a? Array)? @order : @generated.keys
+        list = (@orders.is_a? Array) ? @order : @generated.keys
         list.each do |name|
           records = @generated[name]
           records.each do |record|
-            belong = record.dig(:belong,:to)
-            by = record.dig(:belong,:by)
-            pattern = record.dig(:belong,:search_by)
-            if link.is_a? Symbol then
+            belong = record.dig(:belong, :to)
+            by = record.dig(:belong, :by)
+            pattern = record.dig(:belong, :search_by)
+            if belong.is_a? Symbol
               model = Object.const_get(@models[belong][:class]).find_by(**pattern).send by
-              model.create!(record[:data]) unless model.where(@models[belong][:unicity] => record[:data][@models[belong][:unicity]]).exists?
+              unless model.where(@models[belong][:unicity] => record[:data][@models[belong][:unicity]]).exists?
+                model.create!(record[:data])
+              end
             else
               model = Object.const_get(@models[name][:class])
-              model.create!(record[:data]) unless model.where(@models[name][:unicity] => record[:data][@models[name][:unicity]]).exists?
+              unless model.where(@models[name][:unicity] => record[:data][@models[name][:unicity]]).exists?
+                model.create!(record[:data])
+              end
             end
-
           end
-          
         end
       end
-
 
       def drop_database
         ::Mongoid.purge!
